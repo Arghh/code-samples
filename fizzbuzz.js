0a4455af57e494c2ffd0b12ffcb23eda4694a36c
Argo Liivanurm <tammepoeg@gmail.com>
2015-02-03 16:44:38 +0100

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,6 @@
-//the game of FizzBuzz//
+/*The game of FizzBuzz.
+Rules: instead of saying numbers divisible by 3, say "Fizz". And instead of saying numbers divisible by 5, say "Buzz". 
+For numbers divisible by both 3 and 5, say "FizzBuzz". "1, 2, Fizz, 4, Buzz"...and so forth.*/
  for (i = 1; i <= 20; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz");
