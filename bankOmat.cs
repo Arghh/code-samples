86b7655038e031cf4c92d5b26c55125e3a2d6f0b
Argo Liivanurm <tammepoeg@gmail.com>
2015-08-12 19:03:50 +0200

    vs practice

---
+++ bankOmat.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace BankOmant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //TODO dont accept negative number. restart programm. test values. make a function. 
+            //if user enters letters instead of numbers give error
+            try
+            {
+                int m;
+                string sum;
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                Console.WriteLine("How many € would you like to withdrawal today?");
+                sum = Console.ReadLine();
+                m = Int32.Parse(sum);
+
+                //if the entered amout does not devide with 5
+                if (m % 5 != 0)
+                {
+                    Console.WriteLine("We only have 50€, 20€, 10€ and 5€ banknotes available");
+                    Console.WriteLine(m + " € is not a correct amount.");
+                }
+                //if user entered a correct amout
+                else
+                {
+                    //take the sum amout(m) and fits it into 50,20,10 and 5. shows how many of which notes were given. 
+                    int firstRemainder; //remainders are the leftover amout
+                    int secondRemainder;
+                    int thirdRemainder;
+                    int fourthRemainder;
+
+                    int numOf50s = (Math.DivRem(m, 50, out firstRemainder));
+                    if (numOf50s != 0)
+                    {
+                        Console.WriteLine("You received " + numOf50s + " 50€");
+                    }
+
+
+                    int numOf20s = (Math.DivRem(firstRemainder, 20, out secondRemainder));
+                    if (numOf20s != 0)
+                    {
+                        Console.WriteLine("You received " + numOf20s + " 20€");
+                    }
+
+
+                    int numOf10s = (Math.DivRem(secondRemainder, 10, out thirdRemainder));
+                    if (numOf10s != 0)
+                    {
+                        Console.WriteLine("You received " + numOf10s + " 10€");
+                    }
+
+
+                    int numOf5s = (Math.DivRem(thirdRemainder, 5, out fourthRemainder));
+                    if (numOf5s != 0)
+                    {
+                        Console.WriteLine("You received " + numOf5s + " 5€");
+                    }
+
+                    Console.WriteLine("Have a nice day!");
+
+                }
+            }
+
+
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ fahrToCel.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Globalization;
+
+namespace FtoC
+{
+    class Program
+    {
+        static void Main()
+        {
+            string celius = "";
+            string userChoice = "";
+            var culture = new CultureInfo("de-DE");
+            while (userChoice != "1" || userChoice != "2") //loop so long until user picks correctly
+            {
+                Console.WriteLine("Press C to convert temperatur from Celsius to Fahrenheit");
+                Console.WriteLine("Press F to convert temperatur from Fahrenheit to Celsius");
+                userChoice = Console.ReadLine();
+                if (userChoice.ToUpper() == "F") //celsius to fahrenheit converter
+                {
+                    Console.WriteLine("Please enter the temperatur in Celsius you want to convert to Fahrenheit.");
+                    celius = Console.ReadLine();
+                    double c;
+                    bool celiusToFahren = double.TryParse(celius, NumberStyles.Any, culture, out c); //check if only numbers are entered. convert , to . correct culture
+
+                    if (celiusToFahren == false)
+                    {
+                        Console.WriteLine("Please use numbers only");
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        double answer = ((9.0 / 5.0) * c) + 32; //math from c to f.
+                        Console.WriteLine(c + " °C is " + answer + " °F.");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                    }
+                }
+
+                else if (userChoice.ToUpper() == "C") //fahrenheit to celsius converter
+                {
+                    Console.WriteLine("Please enter the temperatur in Fahrenheit you want to convert to Celsius.");
+                    string fahrenheit = Console.ReadLine();
+                    double f;
+                    bool fahrenToCelsius = double.TryParse(fahrenheit, NumberStyles.Any, culture, out f);
+
+                    if (fahrenToCelsius == false)
+                    {
+                        Console.WriteLine("Please use numbers only.");
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        double answer = (5.0 / 9.0) * (f - 32);
+                        Console.WriteLine(f + " °F is " + answer + " °C.");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                    }
+                }
+
+                else //to deal with all other cases that user enters.
+                {
+                    Console.WriteLine("Oops! Incorrect number. Please try again.");
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
