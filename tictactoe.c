098ff7465eeabe62d14f4d67816e03061bcf8b4e
Argo Liivanurm <tammepoeg@gmail.com>
2015-08-28 15:55:20 +0200

    Create tictactoe.c
    simple tictactoe vs computer.

---
+++ tictactoe.c
@@ -0,0 +1,159 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+//simple tic tac toe user vs computer based on http://www.java2s.com/Code/C/Data-Type/AsimpleTicTacToegame.htm
+
+char board[3][3];
+
+void createBoard()
+{
+  int x, y;
+  for (x = 0; x < 3; x++)
+  for (y = 0; y < 3; y++)
+    board[x][y] = ' ';
+}
+
+void displayBoard()
+{
+  int d;
+  printf("  | 0 | 1 | 2 | \n");
+  for (d = 0; d < 3; d++)
+  {
+     printf("%d | %c | %c | %c | ", d, board[d][0], board[d][1], board[d][2]);
+     printf("\n");
+  }
+}
+
+void userMove()
+{
+  int x, y;
+  printf("Your move\n");
+  while (scanf("%d, %d", &x, &y) != 2)
+  {
+   while (getchar() != '\n');
+    printf ("Enter a row: 0-2 and a column 0-2: like so (0,0)\n");
+  }
+    if(board[x][y] != ' ')
+    {
+      printf("Oops! Try again\n");
+    }
+    else 
+    {
+      board[x][y] = 'X';
+    }
+  
+}
+void computerMove()
+{
+  int x;
+  int y;
+  int boolean;
+  boolean = 0;
+
+  for (y= 0; y < 3; y++)
+  {
+   if (boolean)
+       break;
+   if (board[1][1] == ' ')
+     {
+       board[1][1] = 'O';
+       break;
+     }
+   for (x= 3; x > 0; x--)
+   {    
+     if (board[x][y] == ' ')
+     {
+       board[x][y] = 'O';
+       boolean=1;
+       break;
+     }
+   }
+  }
+}
+char checkWinner()
+{
+  //rows 3x, col 3x, diagonals
+  int i;
+
+  for(i=0; i<3; i++)//check rows
+    {
+      if(board[i][0]==board[i][1] && board[i][0]==board[i][2]) 
+        {
+          return board[i][0];
+        }
+    }
+
+  for(i=0; i<3; i++)//check column
+  {
+    if(board[0][i]==board[1][i] && board[0][i]==board[2][i]) 
+       {
+         return board[0][i];
+       }
+  }
+
+  //test diagonals
+  if(board[0][0]==board[1][1] && board[1][1]==board[2][2])
+  {
+       return board[0][0];
+  }
+
+  if(board[0][2]==board[1][1] && board[1][1]==board[2][0])
+  {
+       return board[0][2];
+  }
+
+  return ' ';
+ /* int i;
+  for (i = 0; i < 3; i++)
+  {
+    if(board[i][0] ==  board[i][1] == board[i][2]) //why wont work
+
+    {
+      return board[i][0];
+    }
+    if(board[0][i] == board[1][i] && board[2][i])
+    {
+      return board[0][i];
+    }
+  }
+  return ' ';*/
+}
+void play()
+{
+  createBoard();
+  do
+  {
+    displayBoard();
+    userMove();
+    checkWinner();
+    if(checkWinner() != ' ')
+      break;
+    computerMove();
+    checkWinner();
+    if(checkWinner() != ' ')
+      break;
+  } while(checkWinner() == ' ');
+  displayBoard();
+  if(checkWinner() == 'X')
+  { 
+    printf("You won\n");
+  }
+  else
+  {
+    printf("You lost\n");
+  }
+}
+
+void main()
+{
+  int check = 1;
+  do
+  {
+  play();
+  printf("\n");
+  printf("\n");
+  printf("Wanna play again? 1 for YES 0 for NO");
+  scanf("%d", &check);
+  }while(check);
+  system("pause");
+}
